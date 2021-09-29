ce9c527e3be2946bc6b7ac781ddd3aecf22e5ae2
miloszr7 <52283934+miloszr7@users.noreply.github.com>
2021-09-29 14:41:19 +0200

    Update PrivateMessage.java

--- src/main/java/privateMessages/PrivateMessage.java
+++ src/main/java/privateMessages/PrivateMessage.java
@@ -1,82 +1,44 @@
-
-
-package privateMessages;
+package event;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.io.File;
-
 public class PrivateMessage extends ListenerAdapter {
 
     public static void sendPrivateMessage(User member, EmbedBuilder content) {
-        member.openPrivateChannel().queue((channel) ->
-        {
+        member.openPrivateChannel().queue((channel) -> {
             channel.sendMessage(content.build()).queue();
 
-            String messageID = channel.getLatestMessageId();
-
-            channel.deleteMessageById(messageID).queue();
-
+            channel.deleteMessageById(channel.getLatestMessageId()).queue();
         });
     }
 
     public static void sendPrivateMessage(User member, String content) {
-        member.openPrivateChannel().queue((channel) ->
-        {
+        member.openPrivateChannel().queue((channel) -> {
             channel.sendMessage(content).queue();
 
-            String messageID = channel.getLatestMessageId();
-
-            channel.deleteMessageById(messageID).queue();
-
+            channel.deleteMessageById(channel.getLatestMessageId()).queue();
         });
     }
 
-    public static void sendPrivateMessage(User member, EmbedBuilder content, boolean removeLatestMessage) {
-        member.openPrivateChannel().queue((channel) ->
-        {
-            if (removeLatestMessage) {
-                channel.sendMessage(content.build()).queue();
-
-                String messageID = channel.getLatestMessageId();
-
-                channel.deleteMessageById(messageID).queue();
-            } else {
-                channel.sendMessage(content.build()).queue();
+    public static void sendPrivateMessage(User member, EmbedBuilder content, boolean removePreviousMessage) {
+        member.openPrivateChannel().queue((channel) -> {
+            if (removePreviousMessage) {
+                channel.deleteMessageById(channel.getLatestMessageId()).queue();
             }
+            channel.sendMessage(content.build()).queue();
         });
     }
 
-    public static void sendPrivateMessage(Member member, EmbedBuilder embedBuilder, boolean removeLatestMessage) {
-        member.getUser().openPrivateChannel().queue(channel ->
-        {
-            if (removeLatestMessage) {
-                channel.sendMessage(embedBuilder.build()).queue();
-
-                String messageID = channel.getLatestMessageId();
-
-                channel.deleteMessageById(messageID).queue();
-            } else {
-                channel.sendMessage(embedBuilder.build()).queue();
+    public static void sendPrivateMessage(Member member, EmbedBuilder embedBuilder, boolean removePreviousMessage) {
+        member.getUser().openPrivateChannel().queue(channel -> {
+            if (removePreviousMessage) {
+                channel.deleteMessageById(channel.getLatestMessageId()).queue();
             }
+            channel.sendMessage(embedBuilder.build()).queue();
         });
     }
-
-    public static void sendPrivateMessage(User author, File file, String filename, boolean removeLatestMessage) {
-
-        author.openPrivateChannel().queue(privateChannel -> {
-
-            if (removeLatestMessage) {
-
-                privateChannel.sendFile(file, filename).queue();
-
-            }
-
-        });
-
-    }
+    
 }
-
